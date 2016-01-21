1af88ad491503465eeac34ae348b96109fe58e07
Joshua Stoutenburg <jehoshua02@gmail.com>
2016-01-21 08:56:16 -0700

    Install React and Configure Build for JSX

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert('hello');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App = require('./components/App');
+
+ReactDOM.render(
+  <App />,
+  document.body.appendChild(document.createElement('div'))
+);
