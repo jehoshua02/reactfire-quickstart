64567033eaf607dc0f3cc85647f81427ada3269d
Joshua Stoutenburg <jehoshua02@gmail.com>
2016-01-21 08:58:17 -0700

    Convert to ES6 Modules (Instead of CommonJS)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-var React = require('react');
+import React from 'react';
 
 var App = React.createClass({
   render: function () {
@@ -6,4 +6,4 @@ var App = React.createClass({
   }
 });
 
-module.exports = App;
+export default App;
