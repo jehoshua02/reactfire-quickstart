afae9e96f73ca03da4cbb47dbfcfdccc7f4ec49b
Joshua Stoutenburg <jehoshua02@gmail.com>
2016-01-21 17:08:10 -0700

    Wire Up App Component to Firebase

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import Firebase from 'firebase';
+import ReactFireMixin from 'reactfire';
 
 var App = React.createClass({
+  mixins: [ReactFireMixin],
   render: function () {
     return (<h1>Hello</h1>);
+  },
+  componentWillMount: function() {
+    var ref = new Firebase("https://jehoshua02-qs.firebaseio.com/items");
+    this.bindAsArray(ref, "items");
   }
 });
 
