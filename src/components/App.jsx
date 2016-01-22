155778bc4e369e31488b224856bd87a8bced4897
Joshua Stoutenburg <jehoshua02@gmail.com>
2016-01-22 00:57:30 -0700

    Retrieve and Display Data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,20 @@ import ReactFireMixin from 'reactfire';
 var App = React.createClass({
   mixins: [ReactFireMixin],
   render: function () {
-    return (<h1>Hello</h1>);
+    return (
+      <ul>
+        {this.state.items.map((item) => {
+          return <li key={item['.key']}>{item['.value']}</li>;
+        })}
+      </ul>
+    );
   },
   componentWillMount: function() {
     var ref = new Firebase("https://jehoshua02-qs.firebaseio.com/items");
+    ref.push(Math.random());
+    ref.push(Math.random());
+    ref.push(Math.random());
+    ref.push(Math.random());
     this.bindAsArray(ref, "items");
   }
 });
